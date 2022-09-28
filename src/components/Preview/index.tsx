c8e1f8b348cbb78c8554156c701b04d5fea90d56
Manuel <manutepowa@gmail.com>
2022-09-28 22:57:31 +0200

    Remove css modules

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,22 +1,22 @@
 import useUploaderContext from "../../context"
 import { Remove } from "../../icons/Remove"
-import css from "./style.module.css"
+import "./style.css"
 
 export const Preview = () => {
   const { images, removeImage } = useUploaderContext()
   return (
     <>
       {images.length > 0 && (
-        <div className={css.container}>
+        <div className="container">
           {images.map((image, index) => {
             const imageToRender: string =
               image instanceof File ? URL.createObjectURL(image) : image.url
             return (
-              <div key={index} className={css.imageCard}>
-                <span className={css.remove} onClick={() => removeImage(index)}>
+              <div key={index} className="imageCard">
+                <span className="remove" onClick={() => removeImage(index)}>
                   <Remove />
                 </span>
-                <img className={css.thumbnail} src={imageToRender} />
+                <img className="thumbnail" src={imageToRender} />
               </div>
             )
           })}
